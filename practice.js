3fa32e53010e000fb81c526d68b3ff18fd6d6f56
Tim <tim.cartr@gmail.com>
2018-06-26 17:01:32 -0600

    first commit

--- practice.js
+++ practice.js
@@ -19,7 +19,9 @@ var arr = [10,20,30];
 
 //Code Here
 
-
+function first(arr){
+  return arr[0]
+};
 
 ////////// PROBLEM 2 //////////
 
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1]
+};
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for(let i = 0 ; i < arr.length ; i++){
+    alert(family[i])
+  }
+};
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length-1 ; i > -1 ; i--){
+    alert(letters[i])
+  }
+};
 
 
 ////////// PROBLEM 5 //////////
@@ -79,8 +91,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
-
+function evenFinder(nums){
+  var newArr = [];
+  for(let i=0 ; i < nums.length ; i++){
+  if(nums[i] % 2 === 0){
+    newArr.push(nums[i])
+  }
+  }
+  return newArr;
+};
 
 
 
